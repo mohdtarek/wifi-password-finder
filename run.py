cfd9ccfcbdb0ecc21e1602b6f71d4e28a7b5b86e
Mohamed Tarek <86129415+7amoodtarek@users.noreply.github.com>
2021-12-12 16:54:58 +0400

    Update run.py

--- run.py
+++ run.py
@@ -1,37 +1,69 @@
-#WIFI PASS GETTER FOR WINDOWS BY MOHAMED TAREK
-#FOR FOR INFO ABOUT THE SCRIPT PLEASE GO TO README.md
+# WIFI PASS GETTER FOR WINDOWS BY MOHAMED TAREK.
+# FOR FOR INFO ABOUT THE SCRIPT PLEASE GO TO README.md.
 
 import os
 import logging
+from colorama import init, Fore
 
+
+# Basic simple log file configuration to save wifi details.
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s:%(message)s",
     filename="usage.log"
     )
 
-os.system(f"netsh wlan show profiles")
-def pass_getter(network_name):
-    size = network_name.split()
-    if len(size) >= 3:
-        print("please enter a proper network name")
-    script_path = os.getcwd() + "\\temp.txt"
-    os.system(f"netsh wlan show profiles {network_name} key = clear>{script_path}")
-    with open(script_path, "r") as f:               
-        for l in f:
-            if "Key Content" in l:
-                print(f"\n\nShowing key content (password) for {network_name}:\n\nPassword {l[27: ]}")
-                logging.info(f"\nNetwork name: {network_name}\nPassword{l[27: ]}")
-            elif "Absent" in l:
-                print("No security key found")
-    print("\nchoose from the above available network profiles\n", "_" * 50)
-    os.remove(script_path)
-try:
-    while True:        
-        user_inpt = input("\nEnter the wifi network (user profile) to be hacked: ")
-        if __name__ == "__main__":
-            pass_getter(user_inpt)       
-except KeyboardInterrupt:
-    exit("\nEXITED")
-except EOFError:
-    exit("\nEXITED")
+# Colors stored in some variables for shortcuts.
+init(autoreset=True)
+error = Fore.RED
+success = Fore.GREEN
+output = Fore.BLUE
+normal = Fore.YELLOW
+warn = Fore.LIGHTYELLOW_EX
+
+def main():
+    # Display to user
+    os.system(f"netsh wlan show profiles")
+    # For Saving all the stored networks on the Windows PC.
+    profiles_path = os.getcwd() + "\\profiles.txt"
+    os.system(f"netsh wlan show profiles>{profiles_path}")
+
+    def get_pass(network_name):
+        # Checking for validity of network name.
+        size = network_name.split()
+        if len(size) >= 3:
+            print(f"{error}please enter a proper network name")
+        # Checking if the network is not found in the system.
+        with open(profiles_path, 'r') as f:
+            check = f.read().split()
+            if network_name.capitalize() not in check:
+                print("\n{error}Network not found in the system")
+        # For creating a temp file to save the network information.
+        tempfile_path = os.getcwd() + "\\temp.txt"
+        os.system(f"netsh wlan show profiles {network_name} key = clear>{tempfile_path}")
+        # Checking for network security settings and fetching password.
+        with open(tempfile_path, "r") as f:               
+            for l in f:
+                if "Key Content" in l:
+                    print(f"\n\n{success}Successfully done! Showing key content (password) for {network_name}:\n\nPassword {output}{l[27: ]}")
+                    logging.info(f"\nNetwork name: {network_name}\nPassword{l[27: ]}")
+                elif "Absent" in l:
+                    print(f"{error}No security key found")
+        print(f"\n{warn}choose from the above available network profiles\n", "_" * 50)
+        # After getting done with the temp file, we delete it.
+        os.remove(tempfile_path)
+    # Handling exceptions with shortcut keys used to exit the app.
+    try:
+        while True:        
+            user_inpt = input(f"\n{normal}Enter the wifi network (user profile) to be hacked: ")
+            get_pass(user_inpt)       
+    except KeyboardInterrupt:
+        exit("\nEXITED")
+    except EOFError:
+        exit("\nEXITED")
+
+# Cuz y not lol    
+if __name__ == "__main__":
+    main()
+# k one more line
+# yep nicee!
