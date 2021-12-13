e2a1e5546e6b2dc0d308d1f2a4c18b2ae0e0ade1
Mohamed Tarek <86129415+7amoodtarek@users.noreply.github.com>
2021-12-13 16:16:31 +0400

    Script to be used in mac still incomplete

---
+++ mac.py
@@ -0,0 +1,40 @@
+# WIFI PASS GETTER FOR MAC OS BY MOHAMED TAREK.
+# FOR FOR INFO ABOUT THE SCRIPT PLEASE GO TO README.md.
+
+# import os
+import logging
+import sys
+from colorama import init, Fore
+from time import sleep
+
+isMac = False
+# Since the os.system commands in the script are only for mac terminal.
+if sys.platform == "darwin":
+    isMac = True  # Code is unrichable incase you viewing this script on either windows or Linux os device
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s:%(message)s",
+    filename="usage.log"
+    )
+
+# Colors stored in some variables for shortcuts.
+init(autoreset=True)
+error = Fore.RED
+success = Fore.GREEN
+output = Fore.BLUE
+normal = Fore.YELLOW
+warn = Fore.LIGHTYELLOW_EX
+
+def main():
+    print("Script is still under development, sorry for the inconvenience!")
+    sleep(2)
+
+
+# Cuz y not lol    
+if __name__ == "__main__" and isMac:
+    main()
+else:
+    print(f"Please make sure you are running the correct script in the correct terminal/ operating system")
+    sleep(2)
